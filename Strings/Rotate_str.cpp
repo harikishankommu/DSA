b5d0bd8b311a0004453addab65004469ad2b290d
Kommu Hariikishan <kommuharikishan@gmail.com>
2025-11-09 23:25:32 +0530

    DSA

---
+++ Strings/Rotate_str.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool rotate_string(string s,string goal){
+    if(s.size()!=goal.size()){
+        return false;
+    }
+
+    string doubled =s+s;
+    if(doubled.find(goal)!=string::npos){
+        return true;
+    }else{
+        return false;
+    }
+}
+int main() {
+    string s,goal;
+    cin>>s>>goal;
+    cout<<rotate_string(s,goal);
+    return 0;
+}
